3987860c17a52d11d72736f3e0a3e07dfc996031
nebyou16 <nebajhoni22@gmail.com>
2024-02-28 18:52:00 +0300

    feature added

--- React-ToDo-App/src/Form.jsx
+++ React-ToDo-App/src/Form.jsx
@@ -3,7 +3,8 @@ export default function Form(){
     return(
         <>
         <div className='container'>
-            <h1>ToDo App</h1>
+            <img className='logo' src="./src/noteicon.png" alt="Logo" /> <br />
+            
         <input className="input" type="text" /> <br />
         <button className="btn">ADD</button>
         <h2>No Task</h2>
