8277796daf2244f1215df042f495853d8e406d92
nebyou16 <nebajhoni22@gmail.com>
2024-02-28 18:42:50 +0300

    New features Added

---
+++ React-ToDo-App/src/Form.jsx
@@ -0,0 +1,13 @@
+import './Form.css'
+export default function Form(){
+    return(
+        <>
+        <div className='container'>
+            <h1>ToDo App</h1>
+        <input className="input" type="text" /> <br />
+        <button className="btn">ADD</button>
+        <h2>No Task</h2>
+        </div>
+        </>
+    )
+}
