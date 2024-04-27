58658626c5efbd457baa9310d0ba6e7db835172c
nebyou <nebyouyohannes1@gmail.com>
2024-04-27 15:30:31 +0300

    .

--- React-ToDo-App/src/Form.jsx
+++ React-ToDo-App/src/Form.jsx
@@ -1,14 +1,25 @@
-import './Form.css'
-export default function Form(){
-    return(
-        <>
+import { useState } from 'react';
+import React from 'react';
+import './Form.css';
+
+export default function Form() {
+
+    const [task, setTask] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () => {
+        setTasks([...tasks, task]);
+        setTask('');
+    }
+
+    return (
         <div className='container'>
             <img className='logo' src="./src/noteicon.png" alt="Logo" /> <br />
-            
-        <input className="input" type="text" /> <br />
-        <button className="btn">ADD</button>
-        <h2>No Task</h2>
+            <input className="input" type="text" value={task} onChange={(e) => setTask(e.target.value)} /> <br />
+            <button className="btn" onClick={addTask}>ADD</button>
+            {tasks.map((t, index) => (
+                <p key={index}>{t}</p>
+            ))}
         </div>
-        </>
     )
 }
